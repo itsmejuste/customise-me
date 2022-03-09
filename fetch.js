4dff714aafe824651fe14da469f2ce98f44f1821
Anna <annabarellaalonso@gmial.com>
2022-03-10 00:25:00 +0100

    Template hidden

--- fetch.js
+++ fetch.js
@@ -1,4 +1,4 @@
-const url = "https://abakea-7ad0.restdb.io/rest/sopaies?max=2";
+const url = "https://abakea-7ad0.restdb.io/rest/sopaies?max=10";
 
 const options = {
   headers: {
@@ -11,7 +11,7 @@ fetch(url, options)
     if (!response.ok) {
       throw Error(response.statusTextt);
     }
-    return response.jeson();
+    return response.json();
   })
 
   .then((data) => {
@@ -21,3 +21,18 @@ fetch(url, options)
   .catch((e) => {
     console.error("An error occurred:", e.message);
   });
+
+function handleData(soap) {
+  soap.array.forEach((soap) => {
+    consol.log(soap);
+    const template = document.querySelector("template").content;
+    const clone = template.cloneNode(true);
+    clone.querySelector("product_name").texContent = soap.product_name;
+    clone.querySelector("product_price").texContent = soap.product_price;
+    clone.querySelector("product_details").texContent = soap.product_details;
+    clone.querySelector("weight").texContent = soap.weight;
+    clone.querySelector("product_oils").texContent = soap.product_oils;
+    clone.querySelector("pproduct_essential_oils").texContent =
+      soap.product_essential_oils;
+  });
+}
